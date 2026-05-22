3d356042c85baf05bebe32c8cd2aa32caba31c8d
Z User <z@container>
2026-05-22 08:09:44 +0000

    c8aa4598-d832-4a2b-887b-471e2851754c

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://mova.is-a.dev";
+  const baseUrl = "https://movadl.vercel.app";
 
   return [
     {
